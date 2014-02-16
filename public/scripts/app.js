1bbceb065edc8f2069d3d0993e3925f0974b1905
Vadim K <igeeko@gmail.com>
2014-02-17 02:41:57 +0600

    Another batch of updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,25 +27,40 @@ require(['jquery'], function( $ ) {
         console.log('click!');
         $(this).parents('.drawer').toggleClass('active');
     });
-    $('.js-modal-close').bind('click', function(e) {
+
+    var closeModal = function() {
         $('.modal.active').removeClass('active');
         $('body').removeClass('modal-open');
+    };
+
+    $('.modal, .modal-overlay').click(function(event) {
+        modal = $(this);
+        // if clicked anywhere else than modal body
+        if (!$(event.target).closest('.box').length) {
+            closeModal();
+        }
+    });
+    $('.js-modal-close').bind('click', function(e) {
+        closeModal();
     });
     $('.js-modal-open').bind('click', function(e) {
         e.preventDefault();
         // close previously opened modal
         $('.modal.active').removeClass('active');
-
+        // which modal to show?
         var tgt = '#'+$(this).attr("data-target");
         $('body').toggleClass('modal-open');
         $(tgt).toggleClass('active');
-        console.log(tgt);
     });
     $(document).keyup(function(e){
         var $bd = $("body");
         if(e.keyCode === 27 && $bd.hasClass('modal-open')) {
-            $('.modal.active').removeClass('active');
-            $('body').removeClass('modal-open');
+            closeModal();
         }
+    });
+
+    $(".js-acc-list .acc-list-item").bind('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
     });
 });
