500d75dfb7f0baa7372bbc2b27db956102aca313
Askhat Omarov <askhat.omarov91@gmail.com>
2014-05-04 18:21:36 +0600

    style sync script changed

--- sync_styles.sh
+++ sync_styles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git pull
 scp -P 3715 -i ~/.ssh/id_rsa -r ~/work/altel/converted-html/public/* root@altel.v3na.kz:/var/www/altel/bitrix/templates/altel/public
+scp -i ~/.ssh/id_rsa -r ~/work/altel/converted-html/public/* wwwadmin@10.254.2.19:/var/www/html/bitrix/templates/altel/public
